4c643ba9670e358636b4ac909d7acec1f179cd71
Malte Hedderich <malte@hedderich.pro>
2025-10-12 11:59:36 +0200

    feat: Add initial implementation of a map to store and print color hex codes

---
+++ udemy/the-complete-developers-guide/5-map/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		println("Hex code for", color, "is", hex)
+	}
+}
