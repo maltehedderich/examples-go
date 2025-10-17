48ea4a688c14dd9b797eba40ec262fe8e25812b1
Malte Hedderich <malte@hedderich.pro>
2025-10-17 21:39:18 +0200

    refactor: Replace ListenAndServe with custom HTTP server configuration

--- learning-cloud-native-go/myapp/main.go
+++ learning-cloud-native-go/myapp/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", hello)
 
-	http.ListenAndServe(":8080", mux)
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+		IdleTimeout:  5 * time.Second,
+	}
+
+	s.ListenAndServe()
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
